c0967b7e3f9435d4f4eb553f337fb29096f9f8dc
Sebastian Vallespir <dsvallespir@gmail.com>
2026-01-05 22:34:39 -0300

    corrección de CMakeLists.txt

--- ex01_pkg/install/ex01_pkg/share/ex01_pkg/cmake/ex01_pkgConfig.cmake
+++ ex01_pkg/install/ex01_pkg/share/ex01_pkg/cmake/ex01_pkgConfig.cmake
@@ -36,7 +36,7 @@ endif()
 set(ex01_pkg_FOUND_AMENT_PACKAGE TRUE)
 
 # include all config extra files
-set(_extras "")
+set(_extras "ament_cmake_export_dependencies-extras.cmake")
 foreach(_extra ${_extras})
   include("${ex01_pkg_DIR}/${_extra}")
 endforeach()
